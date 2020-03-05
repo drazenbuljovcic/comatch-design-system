dc6f560250c41c4a8787bab1d3b89b92a9a301a2
Stanimir Stoychev <xstukixs@gmail.com>
2020-03-05 12:30:27 +0100

    Add theme/palette

---
+++ src/theme/palette.ts
@@ -0,0 +1,149 @@
+const whiteColor = '#FFFFFF';
+const blackColor = '#000000';
+
+/**
+ * primary button color, links color
+ */
+const blue = {
+    color: '#0095B3',
+    font: whiteColor,
+};
+
+/**
+ * graphs, tables
+ */
+const lightBlue = {
+    color: '#E7F7F9',
+    font: blackColor,
+};
+
+/**
+ * hover state
+ */
+const hover = {
+    color: '#3AADC5',
+    font: whiteColor,
+};
+
+/**
+ * pressed state
+ */
+const darkBlue = {
+    color: '#056679',
+    font: whiteColor,
+};
+
+/**
+ * background color
+ */
+const background = {
+    color: '#F7FBFD',
+    font: blackColor,
+};
+
+/**
+ * card and paper background
+ */
+const white = {
+    color: whiteColor,
+    font: blackColor,
+};
+
+/**
+ * primary text color
+ */
+const black = {
+    color: blackColor,
+    font: whiteColor,
+};
+
+/**
+ * secondary text color
+ */
+const gray = {
+    color: '#777777',
+    font: whiteColor,
+};
+
+/**
+ * disabled text, buttons and outlines
+ */
+const lightGray = {
+    color: '#DCE2E2',
+    font: blackColor,
+};
+
+/**
+ * success alerts, labels
+ */
+const green = {
+    color: '#1B7038',
+    font: whiteColor,
+};
+
+/**
+ * success alerts, labels background
+ */
+const lightGreen = {
+    color: '#F6FFFA',
+    font: blackColor,
+};
+
+/**
+ * warning alerts, labels
+ */
+const yellow = {
+    color: '#F7B500',
+    font: whiteColor,
+};
+
+/**
+ * warning alerts, labels background
+ */
+const lightYellow = {
+    color: '#FFFBEE',
+    font: blackColor,
+};
+
+/**
+ * warning alerts, labels text
+ */
+const darkYellow = {
+    color: '#CE7001',
+    font: whiteColor,
+};
+
+/**
+ * error alerts, labels
+ */
+const red = {
+    color: '#AD1919',
+    font: whiteColor,
+};
+
+/**
+ * error alerts, labels background
+ */
+const lightRed = {
+    color: '#FFF8F8',
+    font: blackColor,
+};
+
+export {
+    background,
+    black,
+    blue,
+    darkBlue,
+    darkYellow,
+    gray,
+    green,
+    hover,
+    lightBlue,
+    lightGray,
+    lightGreen,
+    lightRed,
+    lightYellow,
+    red,
+    white,
+    yellow,
+};
