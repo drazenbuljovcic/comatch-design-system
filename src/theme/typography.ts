092dedd764933f624bead77418ed8da85a3cf5e9
Stanimir Stoychev <xstukixs@gmail.com>
2020-03-05 12:36:44 +0100

    Add theme/typography

---
+++ src/theme/typography.ts
@@ -0,0 +1,36 @@
+const headline1 = {
+    fontSize: '24px',
+    lineHeight: 1.5,
+};
+
+const headline2 = {
+    fontSize: '20px',
+    lineHeight: 1.5,
+};
+
+const subHeadline1 = {
+    fontSize: '18px',
+    lineHeight: 1.5,
+};
+
+const subHeadline2 = {
+    fontSize: '16px',
+    lineHeight: 1.5,
+};
+
+const body = {
+    fontSize: '14px',
+    lineHeight: 1.5,
+};
+
+const label = {
+    fontSize: '12px',
+    lineHeight: 1.5,
+};
+
+const footNote = {
+    fontSize: '10px',
+    lineHeight: 1.5,
+};
+
+export { body, footNote, headline1, headline2, label, subHeadline1, subHeadline2 };
