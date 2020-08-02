459c001ce69cde979cd7444ac9003f99d28360b2
Hassan Natera <h.natera@comatch.com>
2020-08-02 20:14:29 +0200

    Add info to Label story

--- src/components/stories/Label.stories.tsx
+++ src/components/stories/Label.stories.tsx
@@ -10,7 +10,7 @@ export default {
 export const Intro = () => (
     <PageWrapper>
         <h1>Label</h1>
-        <p>TBD</p>
+        <p>Label is used when a specific element within a component needs a label</p>
     </PageWrapper>
 );
 
