fa5f7708c75d86392f916bbd6e15a8f46b5cd5fb
Hassan Natera <h.natera@comatch.com>
2020-08-02 20:13:25 +0200

    Add info to Checkbox story

--- src/components/stories/Checkbox.stories.tsx
+++ src/components/stories/Checkbox.stories.tsx
@@ -9,19 +9,21 @@ export default {
 
 export const Intro = () => (
     <PageWrapper>
-        <h1>Checkbox</h1>
-        <p>TBD</p>
+        <h3>Checkbox</h3>
+        <p>Checkboxes should be used when multiple options can be selected from a list.</p>
     </PageWrapper>
 );
 
 export const Default = () => (
     <PageWrapper>
+        <h3>Default Checkbox</h3>
         <Checkbox name="test" />
     </PageWrapper>
 );
 
 export const CheckedByDefault = () => (
     <PageWrapper>
+        <h3>Checked by Default</h3>
         <Checkbox checked name="test" />
     </PageWrapper>
 );
