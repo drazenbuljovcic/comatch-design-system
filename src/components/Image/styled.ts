9af41ac6789bfc2afb75b36dbef6f7391eed9abe
Hassan Natera <h.natera@comatch.com>
2020-08-05 20:33:30 +0200

    Change avatar border color

--- src/components/Image/styled.ts
+++ src/components/Image/styled.ts
@@ -2,11 +2,13 @@ import styled from 'styled-components';
 import { runCSSGeneratorFunction, WithCssGeneratorFunction } from '../../core';
 
 const Wrapper = styled.img<WithCssGeneratorFunction & { avatar: boolean }>`
-    ${({ avatar, theme: { spacing } }) => `
-        ${avatar ? 'border-radius: 50%; width: 100px; height: 100px;' : `margin: ${spacing(4)};`}
-    `}
+    border: solid 1px;
+    border-color: ${({ theme: { palettes } }) => palettes.greyScale.main.bgColor};
+
+    ${({ avatar, theme: { spacing } }) =>
+        avatar ? 'border-radius: 50%; width: 100px; height: 100px;' : `margin: ${spacing(4)};`}
+
     ${runCSSGeneratorFunction}
-    border: solid 1px lightgray;
 `;
 
 export { Wrapper };
