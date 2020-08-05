becc597ed315c27451c8417eb278c05a79459db9
Hassan Natera <h.natera@comatch.com>
2020-08-05 20:31:12 +0200

    Add button titles and adjust story layout

--- src/components/stories/Button.stories.tsx
+++ src/components/stories/Button.stories.tsx
@@ -10,6 +10,15 @@ const PageWrapper = styled(DefaultPageWrapper)`
     }
 `;
 
+const ButtonWrapper = styled.div`
+    display: flex;
+    text-align: center;
+
+    div {
+        margin-right: 50px;
+    }
+`;
+
 export default {
     title: 'Components/Button',
     component: Button,
@@ -18,44 +27,72 @@ export default {
 export const Intro = () => (
     <PageWrapper>
         <h1>Button</h1>
-        <p>TBD</p>
+        <p>
+            In general buttons can contain a combination of a clear text and an icon while links are always text only.
+            Primary buttons are used for only the main actions on a page. Actions that are possible to do, but are not
+            the main focus on the page are displayed as a secondary button.{' '}
+        </p>
     </PageWrapper>
 );
 
 export const Default = () => (
     <PageWrapper>
-        <Button>Find your match</Button>
-        <Button disabled>Find your match</Button>
+        <ButtonWrapper>
+            <div>
+                <h3>Default</h3>
+                <Button>Find your match</Button>
+            </div>
+            <div>
+                <h3>Disabled Default</h3>
+                <Button disabled>Find your match</Button>
+            </div>
+        </ButtonWrapper>
     </PageWrapper>
 );
 
 export const Secondary = () => (
     <PageWrapper>
-        <Button secondary>Find your match</Button>
-        <Button disabled secondary>
-            Find your match
-        </Button>
+        <ButtonWrapper>
+            <div>
+                <h3>Secondary</h3>
+                <Button secondary>Find your match</Button>
+            </div>
+            <div>
+                <h3>Secondary Disabled</h3>
+                <Button disabled secondary>
+                    Find your match
+                </Button>
+            </div>
+        </ButtonWrapper>
     </PageWrapper>
 );
 
 export const WithClickListener = () => (
     <PageWrapper>
-        <Button
-            onClick={(event) => {
-                alert('You clicked the "non-disabled" button');
-                console.log(event);
-            }}
-        >
-            Find your match
-        </Button>
-        <Button
-            disabled
-            onClick={(event) => {
-                alert('You clicked the "disabled" button');
-                console.log(event);
-            }}
-        >
-            Find your match
-        </Button>
+        <ButtonWrapper>
+            <div>
+                <h3>With Click Listener</h3>
+                <Button
+                    onClick={(event) => {
+                        alert('You clicked the "non-disabled" button');
+                        console.log(event);
+                    }}
+                >
+                    Find your match
+                </Button>
+            </div>
+            <div>
+                <h3>With Click Listener Disabled</h3>
+                <Button
+                    disabled
+                    onClick={(event) => {
+                        alert('You clicked the "disabled" button');
+                        console.log(event);
+                    }}
+                >
+                    Find your match
+                </Button>
+            </div>
+        </ButtonWrapper>
     </PageWrapper>
 );
